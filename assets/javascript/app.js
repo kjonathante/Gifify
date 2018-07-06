a3fbe9902ac3f69c5b938abf02156f0de495558c
Kit Jonathan Te <kjonathante@gmail.com>
2018-07-06 13:30:50 -0700

    create abstract function for appending the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,14 @@ favoriteGifs();
 
 
 function favoriteGifs() {
-  let arr=[];
+  //use localStorage to implement favorites
+  let arr;
   for ( let i = 0, len = localStorage.length; i < len; ++i ) {
     console.log(  localStorage.key( i ) );
     arr.push( localStorage.key( i ) );
   }
+
+  if (!arr) return; // short circuit. there is no favorites
 
   const ids = arr.join(',');
   const gifByIDEndPoint = 'https://api.giphy.com/v1/gifs'
@@ -30,37 +33,7 @@ function favoriteGifs() {
   .then( function(res) {
 
     $('#favorites').empty();
-
-    for (let val of res.data) {
-      let id = val.id;
-      //let title = val.title;
-      let rating = val.rating;
-      let still = val.images.fixed_height_small_still.url;
-      let animate = val.images.fixed_height_small.url;
-      let width = val.images.fixed_height_small.width;
-      
-      width = parseInt(width) + 12;
-
-      //let $title = $('<div>').text( title );
-      let $rating = $('<p>').text( 'Rating: ' + rating );
-      let $favButton = $('<span>').addClass('fav').text('fav').attr({
-        "data-id": id
-      });
-      $rating.append( $favButton );
-
-      let $imgDiv = $('<div>').addClass( 'img-holder' ).css({ width: width });
-      let $img = $('<img>').attr({
-        src: still,
-        "data-still": still,
-        "data-animate": animate,
-        "data-state": "still",
-        class: "gif"
-      });
-      $imgDiv.append( $img, $rating );
-
-      $('#favorites').append( $imgDiv );
-    }
-
+    appendGifs( res.data, '#favorites');
 
   });
 }
@@ -70,19 +43,21 @@ function favoriteGifs() {
 
 
 
-
+// Search Giphy when a button is click
 $(document).on('click', '#btn_div button', function(){
-
   const searchEndPoint = 'https://api.giphy.com/v1/gifs/search'
   const apiKey = '53oJ4FxBUFiyz17NiydseLl7lRaUAiyd';
   const keyword = $(this).text();
 
+  // Retrieve object literal using .data()
   const data = $('#btn_div').data();
+  // get the previous offset and add the previous count if the same button is click
   let offset = 0;
   if ((data) && (data.keyword == keyword)) {
     offset = data.offset + data.count;
   }
 
+  // contruct the url needed for this endpoint
   const url = searchEndPoint + '?' + $.param({
     api_key: apiKey,
     q: encodeURI( keyword ),
@@ -90,16 +65,20 @@ $(document).on('click', '#btn_div button', function(){
     offset: offset,
     lang: "en"
   });
+  console.log(url);
 
-  $.each( $('#btn_div').data(), function(key,val) {
-    console.log( key, val)
-  });
+  // $.each( $('#btn_div').data(), function(key,val) {
+  //   console.log( key, val)
+  // });
 
   $.ajax({
     url: url
   })
   .then( function(res) {
-    appendGifs( res );
+    if ( res.pagination.offset == 0) {
+      $('#main').empty();
+    }
+    appendGifs( res.data, '#main' );
     $('#btn_div').data({
       keyword: keyword,
       totalCount: res.pagination.total_count,
@@ -109,17 +88,14 @@ $(document).on('click', '#btn_div button', function(){
   });
 });
 
-function appendGifs( res ) {
+function appendGifs( data, target ) {
   //<img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" 
   //     data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" 
   //     data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" 
   //     data-state="still" 
   //     class="gif">
-  if ( res.pagination.offset == 0) {
-    $('#main').empty();
-  }
 
-  for (let val of res.data) {
+  for (let val of data) {
     let id = val.id;
     let rating = val.rating;
     let stillURL = val.images.fixed_height_small_still.url;
@@ -129,7 +105,7 @@ function appendGifs( res ) {
     width =  (width < 100) ? 100 : width + 12;
 
     let $rating = $('<div>').text( 'Rating: ' + rating );
-    let $favButton = $('<div>').addClass('fav').text('fav').attr('data-id', id);
+    //let $favButton = $('<div>').addClass('fav').text('fav').attr('data-id', id);
     //$rating.append( $favButton );
 
     let $imgDiv = $('<div>').addClass( 'img-holder' ).css({ width: width });
@@ -141,18 +117,18 @@ function appendGifs( res ) {
       'class': 'gif'
     });
 
-    $imgDiv.append( $img, $rating, $favButton );
-    $('#main').append( $imgDiv );
+    $imgDiv.append( $img, $rating );
+    //$imgDiv.append( $favButton );
+    $(target).append( $imgDiv );
   }
 }
 
 $('#search').on('click', function(evt) {
   evt.preventDefault();
-  //console.log("preventDefault");
-  let search = $('#query').val().trim();
-  if ( search ) {
-    topics.push( search );
-    createButton( search );
+  let keyword = $('#query').val().trim();
+  if ( keyword ) {
+    topics.push( keyword );
+    createButton( keyword );
     $('#query').val('');
   }
 });
