eb31d2b34b56dc90aeeb7d2963f253a0de7325f5
Kit Jonathan Te <kjonathante@gmail.com>
2018-07-06 10:48:39 -0700

    add fav implementation and refactor some codes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,56 +75,76 @@ $(document).on('click', '#btn_div button', function(){
 
   const searchEndPoint = 'https://api.giphy.com/v1/gifs/search'
   const apiKey = '53oJ4FxBUFiyz17NiydseLl7lRaUAiyd';
+  const keyword = $(this).text();
+
+  const data = $('#btn_div').data();
+  let offset = 0;
+  if ((data) && (data.keyword == keyword)) {
+    offset = data.offset + data.count;
+  }
 
   const url = searchEndPoint + '?' + $.param({
     api_key: apiKey,
-    q: $(this).text(),
+    q: encodeURI( keyword ),
     limit: 10,
+    offset: offset,
     lang: "en"
+  });
+
+  $.each( $('#btn_div').data(), function(key,val) {
+    console.log( key, val)
   });
 
   $.ajax({
     url: url
   })
   .then( function(res) {
-    //<img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" 
-    //     data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" 
-    //     data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" 
-    //     data-state="still" 
-    //     class="gif">
-    $('#main').empty();
-
-    for (let val of res.data) {
-      let id = val.id;
-      //let title = val.title;
-      let rating = val.rating;
-      let still = val.images.fixed_height_small_still.url;
-      let animate = val.images.fixed_height_small.url;
-      let width = val.images.fixed_height_small.width;
-      
-      width = parseInt(width) + 12;
-
-      //let $title = $('<div>').text( title );
-      let $rating = $('<p>').text( 'Rating: ' + rating );
-      let $favButton = $('<span>').addClass('fav').text('fav').attr({
-        "data-id": id
-      });
-      $rating.append( $favButton );
-
-      let $imgDiv = $('<div>').addClass( 'img-holder' ).css({ width: width });
-      let $img = $('<img>').attr({
-        src: still,
-        "data-still": still,
-        "data-animate": animate,
-        "data-state": "still",
-        class: "gif"
-      });
-      $imgDiv.append( $img, $rating );
-
-      $('#main').append( $imgDiv );
-    }
+    appendGifs( res );
+    $('#btn_div').data({
+      keyword: keyword,
+      totalCount: res.pagination.total_count,
+      count: res.pagination.count,
+      offset: res.pagination.offset
+    });
   });
 });
+
+function appendGifs( res ) {
+  //<img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" 
+  //     data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" 
+  //     data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" 
+  //     data-state="still" 
+  //     class="gif">
+  if ( res.pagination.offset == 0) {
+    $('#main').empty();
+  }
+
+  for (let val of res.data) {
+    let id = val.id;
+    let rating = val.rating;
+    let stillURL = val.images.fixed_height_small_still.url;
+    let animateURL = val.images.fixed_height_small.url;
+    let width = parseInt( val.images.fixed_height_small.width );
+    
+    width =  (width < 100) ? 100 : width + 12;
+
+    let $rating = $('<div>').text( 'Rating: ' + rating );
+    let $favButton = $('<div>').addClass('fav').text('fav').attr('data-id', id);
+    //$rating.append( $favButton );
+
+    let $imgDiv = $('<div>').addClass( 'img-holder' ).css({ width: width });
+    let $img = $('<img>').attr({
+      'src': stillURL,
+      'data-still': stillURL,
+      'data-animate': animateURL,
+      'data-state': 'still',
+      'class': 'gif'
+    });
+
+    $imgDiv.append( $img, $rating, $favButton );
+    $('#main').append( $imgDiv );
+  }
+}
 
 $('#search').on('click', function(evt) {
   evt.preventDefault();
